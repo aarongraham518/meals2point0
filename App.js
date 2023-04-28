ea0759932175ce20aaea5a28c493e8b33063f3e1
Aaron Graham <aarongraham@Aarons-Mac-mini.local>
2023-04-28 12:24:02 -0400

    setup files, small components

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+
+import { RestaurantsScreen } from './src/features/restaurants/screens/restaurant.screen';
+// const isAndroid = Platform.OS === 'android';/
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    
+    <RestaurantsScreen/>
+    <ExpoStatusBar style="auto"/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
---
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
+import { Searchbar } from 'react-native-paper';
+import { RestaurantInfo } from '../components/restaurant-info-component';
+
+export const RestaurantsScreen = () => (
+    <SafeAreaView style={styles.container}>
+     <View style={styles.search}>
+      <Searchbar style={styles.searchBar}/>
+    </View>
+    <View style={styles.list}>
+        <RestaurantInfo />
+    </View>
+    </SafeAreaView>
+)
+
+const styles = StyleSheet.create({
+    container:{
+     flex: 1,
+     marginTop: StatusBar.currentHeight,
+     backgroundColor: 'brown'
+   },
+   search: {
+     padding: 16, 
+     backgroundColor: 'green'
+   },
+   searchBar:{
+     borderRadius: '8'
+   },
+   list: {
+     flex: 1, 
+     padding: 16, 
+     backgroundColor: 'blue'
+   }
+   });
+   
