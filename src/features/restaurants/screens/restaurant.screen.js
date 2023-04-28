2e7ccac3c2f0994afd0acce729301a8ac1ac2114
Aaron Graham <aarongraham@Aarons-Mac-mini.local>
2023-04-28 13:06:37 -0400

    created card component

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
 import { Searchbar } from 'react-native-paper';
-import { RestaurantInfo } from '../components/restaurant-info-component';
+import { RestaurantInfoCard } from '../components/restaurant-info-card-component';
 
 export const RestaurantsScreen = () => (
     <SafeAreaView style={styles.container}>
@@ -9,7 +9,7 @@ export const RestaurantsScreen = () => (
       <Searchbar style={styles.searchBar}/>
     </View>
     <View style={styles.list}>
-        <RestaurantInfo />
+        <RestaurantInfoCard />
     </View>
     </SafeAreaView>
 )
