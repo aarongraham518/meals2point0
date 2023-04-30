f59a00ca9cd04f9a3abb6420b9539e9ec34c494a
Aaron Graham <aarongraham@Aarons-Mac-mini.local>
2023-04-29 23:20:41 -0400

    update styles, add flatlist and theme

--- src/features/restaurants/components/restaurant-info-card-component.js
+++ src/features/restaurants/components/restaurant-info-card-component.js
@@ -1,51 +1,20 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, Image, View } from "react-native";
-import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
+import { Spacer } from "../../../components/spacer/spacer-component";
+import { Text } from "../../../components/typography/text-component";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
-import { Spacer } from "../../../components/spacer/spacer.component";
-
-const RestaurantCard = styled(Card)`
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-
-const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[3]};
-  background-color: ${(props) => props.theme.colors.bg.primary};
-`;
-const Address = styled.Text`
-  font-family: ${(props) => props.theme.fonts.body};
-  font-size: ${(props) => props.theme.fonts.caption};
-`;
-
-const Title = styled.Text`
-  font-family: ${(props) => props.theme.fonts.heading};
-  font-size: ${(props) => props.theme.fontSizes.body};
-  color: ${(props) => props.theme.colors.ui.primary};
-`;
-
-const Info = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
-
-const Rating = styled.View`
-  flex-direction: row;
-  padding-top: ${(props) => props.theme.space[2]};
-  padding-bottom: ${(props) => props.theme.space[2]};
-`;
-
-const Section = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-
-const SectionEnd = styled.View`
-  flex: 1;
-  flex-direction: row;
-  justify-content: flex-end;
-`;
+import {
+  RestaurantCard,
+  RestaurantCardCover,
+  Info,
+  Section,
+  SectionEnd,
+  Rating,
+  Icon,
+  Address,
+} from "./restaurant-info-card-styles";
 
 const Open = styled(SvgXml)`
   flex-direction: row;
@@ -65,15 +34,12 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
- 
+
   return (
     <RestaurantCard elevation={5}>
-      <RestaurantCardCover
-        key={name}
-        source={{ uri: photos[0] }}
-      />
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
-        <Title>{name}</Title>
+        <Text variant="label">{name}</Text>
         <Section>
           <Rating>
             {ratingArray.map(() => (
@@ -82,17 +48,16 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
           </Rating>
           <SectionEnd>
             {isClosedTemporarily && (
-              <Text variant="label" style={{ color: "red" }}>
+              <Text variant="error" style={{ color: "red" }}>
                 CLOSED TEMPORARILY
               </Text>
             )}
             <Spacer position="left">
-            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
             </Spacer>
-            
+
             <Spacer position="left" size="large">
-            {/* <View style={{ paddingLeft: 16 }} /> */}
-            <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+              <Icon source={{ uri: icon }} />
             </Spacer>
           </SectionEnd>
         </Section>
@@ -101,4 +66,3 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     </RestaurantCard>
   );
 };
-
--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,52 +1,51 @@
-import React from 'react';
-import styled from 'styled-components/native';
-import { StatusBar, SafeAreaView, StyleSheet, Text, View, Platform } from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import { RestaurantInfoCard } from '../components/restaurant-info-card-component';
+import React from "react";
+import styled from "styled-components/native";
+import { StatusBar, SafeAreaView, StyleSheet, FlatList } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfoCard } from "../components/restaurant-info-card-component";
 
 const SafeArea = styled(SafeAreaView)`
-    flex: 1;
-    //if, has a value, apply margin-top
-    ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+  flex: 1;
+  //if, has a value, apply margin-top
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
-    padding: ${(props) => props.theme.space[3]};
+  padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled.View`
-    flex: 1;
-    padding: ${(props) => props.theme.space[3]};
-    background-color: blue;
-`;
+
 export const RestaurantsScreen = () => (
-    <SafeArea style={styles.container}>
-     <SearchContainer style={styles.search}>
-      <Searchbar style={styles.searchBar}/>
+  <SafeArea style={styles.container}>
+    <SearchContainer style={styles.search}>
+      <Searchbar style={styles.searchBar} />
     </SearchContainer>
-    <RestaurantListContainer style={styles.list}>
-        <RestaurantInfoCard />
-    </RestaurantListContainer>
-    </SafeArea>
-)
+    <FlatList
+      data={[{name: 1}, {name: 2}]}
+      renderItem={() => <RestaurantInfoCard />}
+      keyExtractor={(item) => item.name}
+      contentContainerStyle={{padding: 16}}
+    />
+    
+  </SafeArea>
+);
 
 const styles = StyleSheet.create({
-    container:{
-     flex: 1,
-     marginTop: StatusBar.currentHeight,
-     backgroundColor: 'brown'
-   },
-   search: {
-     padding: 16, 
-     backgroundColor: 'green'
-   },
-   searchBar:{
-     borderRadius: '8'
-   },
-   list: {
-     flex: 1, 
-     padding: 16, 
-     backgroundColor: 'blue'
-   }
-   });
-   
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+    backgroundColor: "brown",
+  },
+  search: {
+    padding: 16,
+    backgroundColor: "green",
+  },
+  searchBar: {
+    borderRadius: "8",
+  },
+  list: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: "blue",
+  },
+});
