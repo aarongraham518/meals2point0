47a9d03303781925622f104a15ce67816658fd55
Aaron Graham <aarongraham@Aarons-Mac-mini.local>
2023-05-01 00:23:08 -0400

    added bottom tab navigation

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import { StyleSheet, Text, View, Platform } from 'react-native';
-import {ThemeProvider} from 'styled-components/native';
-import {theme} from './src/infrastructure/theme';
-import { RestaurantsScreen } from './src/features/restaurants/screens/restaurant.screen';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurant.screen";
+import { SafeArea } from "./src/components/utility/safe-area-component";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import {Ionicons} from "@expo/vector-icons";
+const Tab = createBottomTabNavigator();
 // const isAndroid = Platform.OS === 'android';/
 
 import {
@@ -24,18 +29,59 @@ export default function App() {
     return null;
   }
 
+  function SettingsScreen() {
+    return (
+      <SafeArea>
+        <Text>Settings Screen</Text>
+      </SafeArea>
+    );
+  }
+
+  function MapScreen() {
+    return (
+      <SafeArea>
+        <Text>Map Screen</Text>
+      </SafeArea>
+    );
+  }
   return (
     <>
-    {/* not context api, but styled-componenent */}
-    <ThemeProvider theme={theme}>
-    <RestaurantsScreen/>
-    </ThemeProvider>
-    
-    <ExpoStatusBar style="auto"/>
+      {/* not context api, but styled-componenent */}
+      <ThemeProvider theme={theme}>
+        {/* <RestaurantsScreen/> */}
+        <NavigationContainer>
+          <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ color, size }) => {
+              let iconName;
+
+              if (route.name === "Restaurants") {
+                iconName = "md-restaurant";
+              } else if (route.name === "Settings") {
+                iconName = "md-settings";
+              } else if (route.name === "Map") {
+                iconName = "md-map";
+              }
+
+              // You can return any component that you like here!
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "tomato",
+            inactiveTintColor: "gray",
+          }}
+          >
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,51 +1,40 @@
 import React from "react";
 import styled from "styled-components/native";
-import { StatusBar, SafeAreaView, StyleSheet, FlatList } from "react-native";
+
+import { SafeArea } from "../../../components/utility/safe-area-component";
+import { FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card-component";
-
-const SafeArea = styled(SafeAreaView)`
-  flex: 1;
-  //if, has a value, apply margin-top
-  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
-`;
+import { Spacer } from "../../../components/spacer/spacer-component";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => (
-  <SafeArea style={styles.container}>
-    <SearchContainer style={styles.search}>
-      <Searchbar style={styles.searchBar} />
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
     </SearchContainer>
-    <FlatList
-      data={[{name: 1}, {name: 2}]}
-      renderItem={() => <RestaurantInfoCard />}
+    <RestaurantList
+      data={[
+        { name: 1 },
+        { name: 2 },
+        { name: 3 },
+       
+      ]}
+      renderItem={() => (
+        <Spacer position="bottom" size="large">
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
       keyExtractor={(item) => item.name}
-      contentContainerStyle={{padding: 16}}
     />
-    
   </SafeArea>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-    backgroundColor: "brown",
-  },
-  search: {
-    padding: 16,
-    backgroundColor: "green",
-  },
-  searchBar: {
-    borderRadius: "8",
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: "blue",
-  },
-});
