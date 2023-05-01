3e2c7db441afd36176fccd317c6bdf3ed7ee148e
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-05-01 18:55:00 -0400

    update fake api

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { SafeArea } from "./src/components/utility/safe-area-component";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import {Ionicons} from "@expo/vector-icons";
+
+
 const Tab = createBottomTabNavigator();
 // const isAndroid = Platform.OS === 'android';/
 
