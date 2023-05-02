7d47bba6887c8a7f2a8e79171fbe49a7c910d2ea
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-05-01 22:54:50 -0400

    added loading component

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -3,7 +3,7 @@ import styled from "styled-components/native";
 
 import { SafeArea } from "../../../components/utility/safe-area-component";
 import { FlatList } from "react-native";
-import { Searchbar } from "react-native-paper";
+import { ActivityIndicator, Colors, Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card-component";
 import { Spacer } from "../../../components/spacer/spacer-component";
 
@@ -18,12 +18,26 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   console.log(error, 'is the error');
   console.log(restaurants, '!!!!!!!!!!!!!')
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={"blue"} />
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
