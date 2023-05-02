169e5482204d9b02a986a99cbceeddd2e309d771
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-05-02 18:33:59 -0400

    update search context

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,16 +1,15 @@
 import React, { useContext } from "react";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
+import { ActivityIndicator, Colors } from "react-native-paper";
 
 import { SafeArea } from "../../../components/utility/safe-area-component";
-import { FlatList } from "react-native";
-import { ActivityIndicator, Colors, Searchbar } from "react-native-paper";
-import { RestaurantInfoCard } from "../components/restaurant-info-card-component";
 import { Spacer } from "../../../components/spacer/spacer-component";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants-context";
-const SearchContainer = styled.View`
-  padding: ${(props) => props.theme.space[3]};
-`;
+
+import { Search } from "../components/search.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card-component";
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
@@ -29,22 +28,17 @@ const LoadingContainer = styled.View`
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  // console.log(error, 'is the error');
-  // console.log(restaurants, '!!!!!!!!!!!!!')
   return (
     <SafeArea>
       {isLoading && (
         <LoadingContainer>
-          <Loading size={50} animating={true} color={"blue"} />
+          <Loading size={50} animating={true} color={'blue'} />
         </LoadingContainer>
       )}
-      <SearchContainer>
-        <Searchbar />
-      </SearchContainer>
+      <Search />
       <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
-          // console.log(item, 'IS THE ITEM1!!!!!!!!!');
           return (
             <Spacer position="bottom" size="large">
               <RestaurantInfoCard restaurant={item} />
--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -5,14 +5,18 @@ import { locationRequest, locationTransform } from "./location.service";
 export const LocationContext = createContext();
 
 export const LocationContextProvider = ({ children }) => {
-  const [keyword, setKeyword] = useState("san francisco");
-  const [location, setLocation] = useState([]);
+  const [keyword, setKeyword] = useState("San Francisco");
+  const [location, setLocation] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const onSearch = (searchKeyword) => {
     setIsLoading(true);
     setKeyword(searchKeyword);
+    if (!searchKeyword.length) {
+      // don't do anything
+      return;
+    }
     locationRequest(searchKeyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
@@ -23,6 +27,7 @@ export const LocationContextProvider = ({ children }) => {
       .catch((err) => {
         setIsLoading(false);
         setError(err);
+        console.log(err);
       });
   };
 
