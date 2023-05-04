c0f674a6903eb2dbda143804821d77a19a0ad912
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-05-03 22:40:38 -0400

    building sub navigational stacks

---
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {createStackNavigator} from '@react-navigation/stack';
+
+const RestaurantStack = createStackNavigator();
+
+const RestaurantsNavigator = () => {
+    return (
+        <RestaurantStack.Navigator>
+            
+        </RestaurantStack.Navigator>
+    )
+}
