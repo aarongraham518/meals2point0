fe9dab42204a770a355ea54a7220ac46661bf9a6
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-05-04 23:54:01 -0400

    images and navigation added to markers on map

--- src/features/map/screens/map.screen.js
+++ src/features/map/screens/map.screen.js
@@ -1,20 +1,20 @@
 import React, { useContext, useState, useEffect } from "react";
 import MapView from "react-native-maps";
-import {Marker} from 'react-native-maps';
+import { Marker } from "react-native-maps";
 import styled from "styled-components/native";
-
+import { View, Text, Touchable, TouchableOpacity } from "react-native";
 import { LocationContext } from "../../../services/location/location.context";
 import { RestaurantsContext } from "../../../services/restaurants/restaurants-context";
 
 import { Search } from "../components/search.component";
+import { MapCallout } from "../components/map-callout.component";
 
 const Map = styled(MapView)`
   height: 100%;
   width: 100%;
 `;
 
-
-export const MapScreen = () => {
+export const MapScreen = ({navigation}) => {
   const { location } = useContext(LocationContext);
   const { restaurants = [] } = useContext(RestaurantsContext);
 
@@ -49,7 +49,14 @@ export const MapScreen = () => {
                 latitude: restaurant.geometry.location.lat,
                 longitude: restaurant.geometry.location.lng,
               }}
-            />
+            >
+              
+                <TouchableOpacity onPress={() => navigation.navigate("RestaurantDetail", {
+                    restaurant: restaurant,
+                })}>
+                 <MapCallout restaurant={restaurant}/>
+                </TouchableOpacity>
+            </Marker>
           );
         })}
       </Map>
