b5282e21b354b2308a2c55b72307300b0da9459b
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-05-08 23:16:44 -0400

    opacity screen overlay added

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -2,7 +2,6 @@ import React from "react";
 import { AccountBackground, AccountCover } from "../components/account.styles";
 
 export const AccountScreen = () => {
-  return <AccountBackground />;
   return (
     <AccountBackground>
       <AccountCover />
