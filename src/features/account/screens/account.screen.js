c5a2ad4835c4f21c6bb240235fe4659c73050ef9
Aaron Graham <aarongraham@Aarons-Mac-mini.lan>
2023-05-08 23:06:42 -0400

    image and default screens added

---
+++ src/features/account/screens/account.screen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { AccountBackground, AccountCover } from "../components/account.styles";
+
+export const AccountScreen = () => {
+  return <AccountBackground />;
+  return (
+    <AccountBackground>
+      <AccountCover />
+    </AccountBackground>
+  );
+};
